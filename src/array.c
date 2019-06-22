ab852d4ad5e767a1d47c9e8fd64c461db5925069
Takahashi Kosuke <takahashi.k.ci@is-mac45.edu.c.titech.ac.jp>
2019-06-22 22:46:11 +0900

    test1

--- src/array.c
+++ src/array.c
@@ -8,12 +8,19 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  lb = 0;
-  ub = 0;
+  lb = -1;
+  ub = n;
   
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+
+  while(ub - lb > 1){
+      int m = (ub + lb) / 2;
+      if(A[m] >= k) ub = m;
+      else lb = m;
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,22 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int w = 0;
+  int v = 0;
+  for(int i = 0, i < n, i++){
+      if(m < A[i])
+          v = k + 1;
+      if( m >= w + A[i])
+          w += A[i];
+      else{
+          v += 1;
+          w = A[i];
+      }
+        
+  }
+  return v < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +27,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while( ub - lb > 1){
+      int m = (ub + lb) / 2;
+      if(p(m)) ub = m;
+      else lb = m;
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
